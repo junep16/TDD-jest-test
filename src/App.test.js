0b6140b779ed643b4812005149032cb1898229d5
junep16 <jiyunpark0816@gmail.com>
2022-03-26 15:11:28 +0900

    ✅ queryBy 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.queryBy(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  // getBy 둘 이상의 일치요소 발견 시 오류 생성 
+  // queryBy 요소가 없으면 null 반환
+  // findBy 
 });
