76f5d9f345f09f7f5d3b95b9a454d77d05556830
junep16 <jiyunpark0816@gmail.com>
2022-03-28 22:12:39 +0900

    🧪 summart page 테스트 코드 작성

--- src/App.test.js
+++ src/App.test.js
@@ -2,16 +2,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.queryBy(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 
-  const lintTest = screen.getByRole('button', {
-    name: 'lintTest'
-  })
-  expect(lintTest).toHaveTextContent('lintTest'); 
-
-  // getBy 둘 이상의 일치요소 발견 시 오류 생성 
-  // queryBy 요소가 없으면 null 반환
-  // findBy 
-});
+  });   
