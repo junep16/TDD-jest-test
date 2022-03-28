2798419aa3a4d5d8aa80d7482c45092330175ec7
junep16 <jiyunpark0816@gmail.com>
2022-03-28 21:44:34 +0900

    ✅ eslint test

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.queryBy(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 
+  const lintTest = screen.getByRole('button', {
+    name: 'lintTest'
+  })
+  expect(lintTest).toHaveTextContent('lintTest'); 
+
   // getBy 둘 이상의 일치요소 발견 시 오류 생성 
   // queryBy 요소가 없으면 null 반환
   // findBy 
