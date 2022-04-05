258752c2949b94fb58e89f03b76d3f473fc2ec09
junep16 <jiyunpark0816@gmail.com>
2022-04-05 21:32:30 +0900

    ✅ 상품금액, 옵션, 총 금액 useEffect 추가

--- src/contexts/OrderContext.js
+++ src/contexts/OrderContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo, useEffect } from "react";
 
 export const OrderContext = createContext(); 
 
@@ -7,7 +7,38 @@ export function OrderContextProvider(props) {
   const [orderCounts, setOrderCounts] = useState( {
     products: new Map(), 
     options: new Map(), 
+  });
+  
+  const [total, setTotal] = useState({
+    products: 0, 
+    options: 0, 
+    total: 0 
   }); 
+
+  const pricePerItem = {
+    products: 1000, 
+    options: 500
+  }
+
+  const calculateSubtotal = (orderType, orderCounts) => {
+    let optionCount = 0; 
+    for (const count of orderCounts[orderType].values()) {
+      optionCount += count; 
+    }
+    return optionCount * pricePerItem[orderType]
+  }
+
+  useEffect(() => {
+    const productsTotal = calculateSubtotal("products", orderCounts); 
+    const optionsTotal = calculateSubtotal("options", orderCounts); 
+    const total = productsTotal + optionsTotal;
+    setTotal({
+      products: productsTotal, 
+      options: optionsTotal, 
+      total: total, 
+    }); 
+  }, [orderCounts]); 
+
 
   const value = useMemo(() => {
     function updateItemCount(itemName, newItemCount, orderType) {
@@ -17,8 +48,8 @@ export function OrderContextProvider(props) {
       setOrderCounts(newOrderCounts); 
     }
 
-    return [{...orderCounts}, updateItemCount]
-  },[orderCounts]); 
+    return [{...orderCounts, total}, updateItemCount]
+  },[orderCounts, total]); 
 
   return <OrderContext.Provider value={value}  {...props}/> 
 }
